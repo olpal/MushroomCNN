294590cbdb4ee3be80e82f0beb57984d9469ce93
AJ Olpin <ajolpin@hotmail.com>
2017-07-23 11:26:52 -0700

    Added shell scripts to repository

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+scp *.py aolpin@copper.sharcnet.ca:/scratch/aolpin/testing/code
+scp run.sh aolpin@copper.sharcnet.ca:/scratch/aolpin/testing/code
+scp dataprocess.sh aolpin@copper.sharcnet.ca:/scratch/aolpin/testing/code
