924d62420d8c0d5ecd66bec3b90d2902c922840a
AJ Olpin <ajolpin@hotmail.com>
2017-08-14 21:39:40 -0700

    minor structural changes to training class

--- training.py
+++ training.py
@@ -7,64 +7,42 @@ import parameters as hp
 import sys
 import util as u
 import datetime as dt
+import csv
+
+version = 4.0
 
 u.printf ("Starting model execution at {}".format(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 
-
 """Variables"""
-u.printf ("Intializing variables...")
+u.printf ("Intializing list variables...")
 training_losses = []
 training_accuracies = []
 validation_losses = []
 validation_accuracies = []
-dataset_location = "/Users/aolpin/Documents/School/thesis/data-sig/images"
-output_dir = "/Users/aolpin/Documents/School/thesis/results/{}".format(datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
-training_batch_size = 100
-learning_rate=0.001
-dropout = 0.8
-epochs=2000
-convolutional_layer_count = 3
-version=3.4
-neuron_multiplier=40
-convolutional_filter=5
-sigmoid_shift=0.2
+
+u.printf ("Creating model parameters")
+params = hp.ModelParameters()
 
 """Assign passed in variables if they exist"""
 if len(sys.argv) > 1:
     u.printf ("Arguments found, loading...")
-    dataset_location = sys.argv[1]
-    output_dir = sys.argv[2]
-    training_batch_size = int(sys.argv[3])
-    learning_rate = float(sys.argv[4])
-    dropout = float(sys.argv[5])
-    epochs = int(sys.argv[6])
-    convolutional_layer_count = int(sys.argv[7])
-    neuron_multiplier = int(sys.argv[8])
-    convolutional_filter = int(sys.argv[9])
-    sigmoid_shift = float(sys.argv[10])
+    params.dataset_location = sys.argv[1]
+    params.output_dir = sys.argv[2]
+    params.training_batch_size = int(sys.argv[3])
+    params.learning_rate = float(sys.argv[4])
+    params.dropout = float(sys.argv[5])
+    params.epochs = int(sys.argv[6])
+    params.convolutional_layer_count = int(sys.argv[7])
+    params.neuron_multiplier = int(sys.argv[8])
+    params.convolutional_filter = int(sys.argv[9])
 
 """Data sets"""
 u.printf ("Building dataset...")
-data_set = ds.DataSet(dataset_location, output_dir)
-data_set.dp.max_classes = 65536
+data_set = ds.DataSet(params.dataset_location, params.output_dir)
 test_labels, test_data, validation_labels, validation_data, train_labels, train_data = data_set.load_datasets()
-
-
-"""Model parameters"""
-u.printf ("Creating model parameters")
-params = hp.ModelParameters(data_set.dp.max_classes)
-params.training_batch_size=training_batch_size
-params.learning_rate=learning_rate
-params.dropout=dropout
-params.epochs=epochs
-params.convolutional_layer_count = convolutional_layer_count
 params.check_batch_size(len(train_labels),params.training_batch_size)
 params.check_batch_size(len(test_labels),params.testing_batch_size)
-
-"""Memory parameters"""
-u.printf ("Creating memory parameters")
-mem_params = hp.MemoryParameters(params.epochs)
-
+params.generate_model_paths()
 
 """Session Variables"""
 u.printf ("Initializing Neural Network...")
@@ -73,14 +51,14 @@ y = tf.placeholder('float32')
 keep_prob = tf.placeholder(tf.float32)
 
 """Neural Network Variables"""
-cnn = cn.CNN(x, keep_prob, params.convolutional_layer_count, params.image_size, params.number_of_classes, neuron_multiplier, convolutional_filter)
+cnn = cn.CNN(x, keep_prob, params.convolutional_layer_count, params.image_size, params.number_of_classes, params.neuron_multiplier, params.convolutional_filter)
 prediction = cnn.return_network()
 
 cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=prediction, labels=y))
 optimizer = tf.train.AdamOptimizer(params.learning_rate).minimize(cost)
 
 """Overall Accuracy """
-correct_prediction = tf.equal(tf.round(tf.subtract(tf.nn.sigmoid(prediction),sigmoid_shift)), y)
+correct_prediction = tf.equal(tf.round(tf.nn.sigmoid(prediction)), y)
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 """All true Accuracy """
@@ -90,7 +68,7 @@ accuracy_all_true = tf.reduce_mean(all_true)
 
 """Examination code"""
 prediction_truth = correct_prediction
-prediction_true_values = tf.subtract(tf.nn.sigmoid(prediction),sigmoid_shift)
+prediction_true_values = tf.nn.sigmoid(prediction)
 prediction_round_values = tf.round(tf.nn.sigmoid(prediction))
 cost_examination = tf.nn.sigmoid_cross_entropy_with_logits(logits=prediction, labels=y)
 
@@ -158,34 +136,49 @@ def test_network(sess):
 
 def generate_visuals():
     u.printf ("Generating visuals...")
-    with tf.variable_scope('', reuse=True) as scope_conv:
-        #test=tf.get_collection(tf.GraphKeys.VARIABLES)
-        weights = tf.get_variable('dense/kernel')
 
     test_labels_real = data_set.load_label_datafile(test_labels, 0, params.visualize_images)
     test_data_real= data_set.load_image_data(test_data, 0, params.visualize_images)
 
-    classmap = vs.get_map(0, cnn.return_final_convolutional_layer(), params.image_size, weights)
-    vs.visualize_map(sess, classmap, cnn.return_final_convolutional_layer(), test_data_real, test_labels_real,
-                     params.visualize_images, x, y, prediction, cnn.image_size, data_set.dp, prediction_true_values, x, y, keep_prob)
+    vs.visualize_map(sess, test_data_real, test_labels_real, cnn.image_size, data_set.dp,
+                     prediction_true_values, x, y, keep_prob, params)
 
-    vs.visualize_data(sess, test_data_real, test_labels_real, params.visualize_images, prediction_round_values, x, y, keep_prob,
-                      data_set.dp)
+    vs.visualize_data(sess, test_data_real, test_labels_real, prediction_round_values, x, y, keep_prob,
+                      data_set.dp, params)
 
     vs.plot_model_training(training_losses, validation_losses , "Loss", data_set.dp)
     vs.plot_model_training(training_accuracies, validation_accuracies ,"Accuracy", data_set.dp)
 
 
+def save_model(sess):
+    model_saver = tf.train.Saver()
+    model_saver.save(sess, params.saved_model_file_path)
+
+
+def save_variables():
+    try:
+        with open(params.saved_variable_file_path, 'w') as csvfile:
+            writer = csv.writer(csvfile,delimiter=",")
+            writer.writerow([params.number_of_classes,params.neuron_multiplier,params.convolutional_filter,params.convolutional_layer_count])
+    except:
+        u.printf("Unable to write to variable file: {}".format(params.saved_variable_file_path))
+
+
 if __name__ == '__main__':
     u.printf("Executing version {} of the code".format(version))
     u.printf ("Beginning model execution with the following settings\n" \
-          "Convolutional-Layers:{} Epoch-Count:{} Image-Size:{} Batch-Size:{} Learning-Rate:{} Drop-Out:{} Display-Interval:{} Neuron Multiplier:{} Convolutional Filter:{} Sigmoid Shift:{}" \
+          "Convolutional-Layers:{} Epoch-Count:{} Image-Size:{} Batch-Size:{} Learning-Rate:{} Drop-Out:{} "
+              "Display-Interval:{} Neuron Multiplier:{} Convolutional Filter:{}" \
           .format(params.convolutional_layer_count, params.epochs, params.image_size, params.training_batch_size,
-                  params.learning_rate, params.dropout, params.training_display, neuron_multiplier, convolutional_filter, sigmoid_shift))
+                  params.learning_rate, params.dropout, params.training_display, params.neuron_multiplier, params.convolutional_filter))
 
     with tf.Session() as sess:
         train_network(sess)
         test_network(sess)
         generate_visuals()
+        if params.save_model:
+            save_model(sess)
+            save_variables()
+
 
     u.printf ("Finished model execution at {}".format(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
